5f41dd1a441e39f0c38420ca67f2abe4f0cfdb23
hmendoza <hm263@njit.edu>
2020-12-01 20:02:06 -0500

    8. Added React Router

--- src/App.js
+++ src/App.js
@@ -4,12 +4,32 @@ import GlobalStyle from './components/GlobalStyle';
 
 //Import Pages
 import About from './pages/About';
+import Nav from './components/Nav';
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
+
+//Router
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
-      <About />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <About/>
+        </Route>
+
+        <Route path="/work">
+          <OurWork/>
+        </Route>
+      
+        <Route path="/contact">
+          <ContactUs/>
+        </Route>
+
+      </Switch>
     </div>
   );
 }
